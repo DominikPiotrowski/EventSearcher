b2f697baf46b21e09604ecc489eac2aa9eae7db0
Michał Basiński <michal.basinski88@gmail.com>
2019-08-24 13:57:43 +0200

    fix

---
+++ src/main/java/com/dominikpiotrowski/eventSearcher/configs/EventSearchConfig.java
@@ -0,0 +1,15 @@
+package com.dominikpiotrowski.eventSearcher.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class EventSearchConfig {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
